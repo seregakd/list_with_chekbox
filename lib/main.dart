dfb1d1e0b912e8997aca0bb39b2494f123052551
sergey <seregakdg1@gmail.com>
2019-12-23 18:31:16 +0200

    add struckure

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(ListApp());
 
-class MyApp extends StatelessWidget {
+class ListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: HomePage(title: 'List'),
+      home: new HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+//  HomePage({Key key, this.title}) : super(key: key);
+//  final String title;
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -37,26 +33,36 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        title: //Text('List'),
+        Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('chBox'),
+              Text('Select all'),
+              Text('all count'),
+            ]
         ),
+      ),
+      body: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('chBox'),
+                  Text('Select all'),
+                  Text('all count'),
+                ]
+            ),
+     //       new mList(),
+          ]
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
